70ea9ba43acb39afff1f13965d4d40dfb402cdd7
chaejj <bchj513@hotmail.com>
2017-08-09 22:03:21 +1200

    Edited to allow jar file execution as per instruction.

--- src/FileManager/InputReader.java
+++ src/FileManager/InputReader.java
@@ -18,52 +18,25 @@ import App.Vertex;
  *
  */
 public class InputReader {
-	BufferedReader br;
-	String fileName, trimmedFileName = "";
-	//String fixedFileName = "C:\\Projects\\CJ-and-the-Boizzzz\\input\\ex2_in.dot";
-	public InputReader() {
-	}
-	
-	/**
-	 * method that asks user to provide a url for the file and stores the 
-	 * file url in the correct format. 
-	 */
-	private void loadFile() {
-		br = new BufferedReader(new InputStreamReader(System.in)); //waits user to drop the file to terminal
-		try {
-			fileName = br.readLine();
-			// When user drops a file, bash surrounds the file url around quotation marks.
-			// eg. '/media/cj/SHARED/workspace/Java/306-project1/input/ex1_in.dot'
-			trimmedFileName = fileName.replace("\'", "").trim(); // removes quotation marks and trim the whitespace
-		} catch (IOException e) {
-		}
-		while (!trimmedFileName.endsWith(".dot")) { //if user input does not ends with .dot
-			System.out.print("This file is not a dot file. Please drop a .dot file and press Enter.");
-			loadFile(); //repeat until user gives the correct input
-		}
+	String _inputFileName;
+	public InputReader(String inputFileName) {
+		_inputFileName = inputFileName;
 	}
 	/**
 	 * method that reads off the file and stores information into ////data structures////
 	 * 
 	 */
 	public Graph readFile() {
-		
-		// Get user to specify an absolute path for .dot file by typing or drag/drop
-		System.out.println("Drop a dot file here and press Enter.");
-		loadFile(); 
-		File file = new File(trimmedFileName);
-		
-		// Alternately, can hardcode absolute filepath
-		//File file = new File(fixedFileName);
+		File file = new File(_inputFileName);
 
 		Graph graph = null;
 		
-		try { //if file exists
-			BufferedReader br2 = new BufferedReader(new FileReader(file));
+		try { // read file
+			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line = "";
 			Map<String, Vertex> verticesRead = new HashMap<String, Vertex>();
 			List<Edge> edges = new ArrayList<Edge>();
-			while ((line = br2.readLine()) != null) { // read each line and write on console until the end
+			while ((line = br.readLine()) != null) {
 				if (line.endsWith(";")) {
 					line = line.trim(); //Remove leading and trailing whitespace
 					if (line.contains("->")) { //Line is an edge
@@ -83,11 +56,7 @@ public class InputReader {
 			}
 			List<Vertex> vertices = new ArrayList<>(verticesRead.values());
 			graph = new Graph(vertices, edges); //Create graph
-			br2.close();
-		} catch (FileNotFoundException e) { //if file does not exist, get user input again
-			System.out.print("Invalid File url. Try again. ");
-			trimmedFileName = "";
-			readFile();
+			br.close();
 		} catch (IOException e) {
 		}
 		
