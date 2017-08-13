200f9d9e79d401291833cab11d3b981b41d7a633
chaejj <bchj513@hotmail.com>
2017-08-14 08:03:25 +1200

    Cleaned up a mess. Added javadoc comments. Added additional arguments checks.

--- src/App/App.java
+++ src/App/App.java
@@ -1,79 +1,155 @@
 package App;
 
 import java.io.*;
-import java.util.Map;
 
 import FileManager.*;
 import SearchSpace.Schedule;
 import SearchSpace.SearchSpace;
+/**
+ * The main class of the program. Takes input arguments from user input, reads the input file 
+ * and creates a graph, then uses it to create a correct schedule which is written as an output file.
+ * 
+ * @author CJ Bang
+ *
+ */
 public class App {
-	private static Vertex _root;
-	
-	// When running through the eclipse, change this fixedFileName so that you dont have to give arguments each time you run.
-	static String fixedFileName = "C:\\Program Files\\Eclipse\\eclipse\\workspace\\306\\CJ-and-the-Boizzzz\\input\\ex3_in.dot";
-	//String fixedFileName = "C:\\Projects\\CJ-and-the-Boizzzz\\input\\ex1_in.dot";
-	//C:\Users\Andon\Desktop\CJ-and-the-Boizzzz-master\input\ex1_in.dot
+	final String DIRECTORY = "./input/";
+	final String FILENAME_NOT_GIVEN = "Please enter the filename and number of processors as per instruction.";
+	final String INVALID_FILENAME = "File can't be found. The input dot file should be in the same directory as the jar file. Please try again.";
+	final String PROCESSOR_NUMBER_NOT_GIVEN = "Number of Processors is not given. Please indicate the number of processors to schedule the input graph on.";
+	final String INVALID_PROCESSOR = "Number of Processors needs to be given as a digit. ie. 5 instead of five";
+	final String INVALID_OPTION_VALUE = "Selected option has missing value. Please specify your option correctly as per instruction.";
+	final String INVALID_OPTION = "Invalid option: \"-p\", \"-v\", or \"-o\" was not located correctly.";
+	final String CORE_NUMBER_NOT_GIVEN = "Number of Cores for execution needs to be given as a digit. ie. 5 instead of five";
+	final String CONFIRMATION_MESSAGE = "If options are correctly set, Press \"y\" for yes. The program will start executing automatically.\nIf you want to make changes, press any other key to exit and re-execute the file.";
+	int indexOfArguments = 2;
+	String _inputFileName, _outputFileName; 
+	int _noOfProcessors;
+	int _noOfCores = -1;
+	boolean _visualisationOn = false; // by default visualisation is off 
 
-	static String _inputFileName = ""; 
-	static int _noOfProcessors = 4; // for now its set at 2
-	static int _noOfCores = -1;
-	static boolean _visualisationOn = false; // by default visualisation is off 
-	static String _outputFileName = "Output.dot";
+	/**
+	 * Main program. It takes arguments from the user input and pass them to other methods to process. 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		if (args.length > 0) { // When a jar file is being executed
-			try {
-				_inputFileName = args[0];
-				_outputFileName = _inputFileName.substring(0, _inputFileName.length()-4) + "-output.dot";
-				FileReader fr = new FileReader(new File(_inputFileName));
-			} catch (FileNotFoundException e) {
-				System.out.println("File can't be found. The input dot file should be in the same directory as the jar file. Please try again.");
-				System.exit(0);
-			}
-			try {
-				_noOfProcessors = Integer.parseInt(args[1]);
-			} catch (ArrayIndexOutOfBoundsException e1) {
-				System.out.println("Number of Processors is not given. Please indicate the number of processors to schedule the input graph on.");
-				System.exit(0);
-			} catch (NumberFormatException e2) {
-				System.out.println("Number of Processors needs to be given as a digit. ie. 5 instead of five");
-				System.exit(0);
-			}
-			if (args.length > 2) { // When additional optional parameters are passed in
-				for (int i = 2; i < args.length; i++) {
-					try {
-						if (args[i].equals("-p")) {
-							_noOfCores = Integer.parseInt(args[i+1]);
-						}
-						if (args[i].equals("-v")) {
-							_visualisationOn = true;
-						}
-						if (args[i].equals("-o")) {
-							_outputFileName = args[i+1] + ".dot";
-						}
-					} catch (ArrayIndexOutOfBoundsException e1) {
-						System.out.println("Invalid additional arguments. Please double check the arguments.");
-						System.exit(0);
-					} catch (NumberFormatException e2) {
-						System.out.println("Number of Cores for execution needs to be given as a digit. ie. 5 instead of five");
-					}
-				}
-			}
-			confirmOptionsAndExecute();
-		}
-		// When arguments are not passed in ie. through eclipse - use fixedFileName instead of taking argument
-		startExecution(fixedFileName);
+		new App(args);
+	}
+	
+	public App(String[] args){
+		parseArguments(args);
+		confirmOptionsAndExecute();
+	}
+	/**
+	 * Runs scheduler once user confirms the options selected. 
+	 * Firstly program reads the input file using input reader and creates the graph.
+	 * Using the graph, the program creates a correct schedule with the shortest time.
+	 * And it generates an output file for the created schedule. 
+	 */
+	private void startExecution() {
+		InputReader ir = new InputReader(DIRECTORY + _inputFileName);
+		Graph graph = ir.readFile();
+//		SearchSpace searchSpace = new SearchSpace(graph, _noOfProcessors);	
+//		searchSpace.makeSearchSpace();
+//		searchSpace.tempPrintOutLastNodeScheduleInTimeOrder();	
+//		Schedule s = null;
+//		OutputWriter ow = new OutputWriter(_outputFileName);
+//		ow.writeToFile(s);
 	}
 	
 	/**
-	 * This method prints off the selected options before the scheduler starts executing.
+	 * Parses the given arguments and stores them as variables. Invalid argument 
+	 * throws an appropriate exceptions. When an exception gets thrown,
+	 * an appropriate error message is provided and the program is halted.
+	 * @param args
+	 */
+	private void parseArguments(String[] args) {
+		try {
+			_inputFileName = args[0];
+			_outputFileName = _inputFileName.substring(0, _inputFileName.length()-4) + "-output.dot";
+			FileReader fr = new FileReader(new File(DIRECTORY + _inputFileName));
+		} catch (ArrayIndexOutOfBoundsException e){
+			System.out.println(FILENAME_NOT_GIVEN);
+			System.exit(0);
+		} catch (FileNotFoundException e) {
+			System.out.println(INVALID_FILENAME);
+			System.exit(0);
+		}
+		try {
+			_noOfProcessors = Integer.parseInt(args[1]);
+		} catch (ArrayIndexOutOfBoundsException e1) {
+			System.out.println(PROCESSOR_NUMBER_NOT_GIVEN);
+			System.exit(0);
+		} catch (NumberFormatException e2) {
+			System.out.println(INVALID_PROCESSOR);
+			System.exit(0);
+		}
+		if (args.length > 2) { // When additional optional parameters are passed in
+			checkAdditionalOptions(args);
+		}
+	}
+	
+	/**
+	 * Checks the additional option arguments. 
+	 * Depending on the option type, user may or may not need to pass an additional 
+	 * argument to specify option value. Therefore this method selectively checks for 
+	 * each option type and the required arguments.If any part of the arguments is 
+	 * incorrect, an appropriate error message is provided and the program is halted. 
+	 * @param args
+	 */
+	private void checkAdditionalOptions(String[] args) {
+		while (indexOfArguments < args.length) {
+			if (args[indexOfArguments].equals("-p") || args[indexOfArguments].equals("-o")) { //options are correctly given but its not visualisation
+				try {
+					checkAdditionalOptionValue(args[indexOfArguments], args[indexOfArguments+1]);
+				} catch (ArrayIndexOutOfBoundsException e1) {
+					System.out.println(INVALID_OPTION_VALUE);
+					System.exit(0);
+				} catch (NumberFormatException e2) {
+					System.out.println(CORE_NUMBER_NOT_GIVEN);
+					System.exit(0);
+				}
+				indexOfArguments += 2;
+			} else if (args[indexOfArguments].equals("-v")) { // option is visualisation
+				indexOfArguments += 1;
+			} else {
+				System.out.println(INVALID_OPTION);
+				System.exit(0);
+			}				
+		}
+	}
+	
+	/**
+	 * Checks if the correct value was given for the option chosen by user.
+	 * -p should be followed by an integer value.
+	 * -o should be followed by a filename. 
+	 * if values are not given correctly, an appropriate exception gets thrown.
+	 * @param option
+	 * @param value
+	 * @throws ArrayIndexOutOfBoundsException
+	 * @throws NumberFormatException
+	 */
+	private void checkAdditionalOptionValue(String option, String value) throws ArrayIndexOutOfBoundsException, NumberFormatException {
+		if (option.equals("-p")) {
+			_noOfCores = Integer.parseInt(value);
+		}
+		if (option.equals("-o")) {
+			_outputFileName = value + ".dot";
+		}
+	}
+	
+	/**
+	 * Prints off the selected options before the scheduler starts executing.
 	 * User confirms by pressing y which then allows the scheduler to start execution.
 	 * If user does not press y, the program is halt and has to be re-executed.
 	 */
-	private static void confirmOptionsAndExecute() {
+	private void confirmOptionsAndExecute() {
 		System.out.println("*********************************************************************");
 		System.out.println("The Input Graph to be scheduled is: " + _inputFileName);
 		System.out.println("The Number of Processors to be used is: " + _noOfProcessors);
-		if (_noOfCores != -1) {
+		if (_noOfCores == -1) {
+			System.out.println("No of Cores for execution in parallel is: sequential");
+		} else {
 			System.out.println("No of Cores for execution in parallel is: " + _noOfCores);
 		}
 		if (_visualisationOn) {
@@ -83,93 +159,17 @@ public class App {
 		}
 		System.out.println("The Name of Output File is to be: " + _outputFileName);
 		System.out.println("*********************************************************************");
-		System.out.println("If options are correctly set, Press \"y\" for yes. The program will start executing automatically. If you want to make changes, press any other key to exit and re-execute the file.");		
+		System.out.println(CONFIRMATION_MESSAGE);		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		try {
 			if ((line = br.readLine()).equals("y")) {
-				startExecution(_inputFileName);
+				startExecution();
 			} else {
 				System.out.println("exiting...");
 				System.exit(0);
 			}
 		} catch (IOException e) {
 		}		
-	}
-	/**
-	 * Runs scheduler once user confirms the options selected.
-	 * @param fileName
-	 */
-	private static void startExecution(String inputFileName) {
-		InputReader ir = new InputReader(inputFileName);
-		Graph graph = ir.readFile();
-		SearchSpace searchSpace = new SearchSpace(graph, _noOfProcessors);	
-		searchSpace.makeSearchSpace();								// Creates all possible schedules
-		//searchSpace.tempPrintOutSchedules();						// prints out all possible schedules
-		searchSpace.tempPrintOutLastNodeScheduleInTimeOrder();		// prints out schedule for last vertex
-		String output = searchSpace.outputToPrint();
-		OutputWriter ow = new OutputWriter(_outputFileName, inputFileName);
-		ow.writeToFile(output);
-		
-		//setUpChildrenParents(graph);
-		//setUpLevelsOfNodes(graph);	
-
-	}
-
-	private static void setUpChildrenParents(Graph graph) {
-		for (Vertex v : graph.getVertices()) {
-			for (Edge e : graph.getEdges()) {
-				if (!e.getDestination().getParents().contains(e.getSource())) {
-					e.getDestination().addParent(e.getSource());
-				}
-				if (v.equals(e.getSource())) {
-					Vertex child = e.getDestination();
-					v.addChild(child);
-				} 
-			}
-		}
-	}
-	
-	private static void setUpLevelsOfNodes(Graph graph) {
-		for (Vertex v : graph.getVertices()) {
-			System.out.println("The parents of " + v.getName() + " are: " + "");
-			for (Vertex p : v.getParents()) {
-				System.out.println(p.getName());
-			}
-			if (v.getParents().size() == 0) {
-				_root = v;
-				System.out.println("The root node is: " + v.getName());
-			}
-			int lvl = level(_root,v);
-			v.setLevel(lvl);
-			System.out.println("The node " + v.getName() + " resides on level: " + lvl);
-		}
-		
-	}
-	/**
-	 * This helper method topologically sorts a given di-graph.
-	 * As of right now, I(Andon)'m not sure as to whether this method will actually be helpful. But it has been implemented
-	 * as by meeting discussion. Open to any other ideas for generating schedules. 
-	 * @param root : the start node at which to begin tree traversal.
-	 * @param target : the target node that we want to find the height of.
-	 * @return
-	 */
-	public static Integer level(Vertex root, Vertex target){
-	    return level(root, target, 0);
-	}
-
-	private static Integer level(Vertex tree, Vertex target, int currentLevel) {
-	    Integer returnLevel = -1;        
-	    if(tree.getName().equals(target.getName())) {
-	        returnLevel = currentLevel;
-	    } else {
-	        for(Vertex child : tree.getChildren()) {
-	            if((returnLevel = level(child, target, currentLevel + 1)) != -1){
-	                break;
-	            }                
-	        }
-	    }
-	    return returnLevel;
-
 	}
 }
