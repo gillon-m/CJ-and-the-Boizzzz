f06baa4bdbe673ab974b1d6fd45025a22dc36aee
Brad Miller <bmil852@UoA.auckland.ac.nz>
2017-08-19 17:48:09 +1200

    Concurrency working with SwingWorker

--- src/TaskScheduler.java
+++ src/TaskScheduler.java
@@ -5,15 +5,15 @@ import graph.Graph;
 import scheduler.Schedule;
 import scheduler.Scheduler;
 /**
- * The main class of the program. Takes input arguments from user input, reads the input file 
+ * The main class of the program. Takes input arguments from user input, reads the input file
  * and creates a graph, then uses it to create a correct schedule which is written as an output file.
- * 
+ *
  * @author CJ Bang
  *
  */
 public class TaskScheduler {
 	//final String DIRECTORY = "./input/";
-	
+
 	// strings for error messages
 	final String FILENAME_NOT_GIVEN = "Please enter the filename and number of processors as per instruction.";
 	final String INVALID_FILENAME = "File can't be found. The input dot file should be in the same directory as the jar file. Please try again.";
@@ -24,53 +24,53 @@ public class TaskScheduler {
 	final String CORE_NUMBER_NOT_GIVEN = "Number of Cores for execution needs to be given as a digit. ie. 5 instead of five";
 	final String CONFIRMATION_MESSAGE = "If options are correctly set, Press \"y\" for yes. The program will start executing automatically.\nIf you want to make changes, press any other key to exit and re-execute the file.";
 
-	
+
 	int indexOfArguments = 2; //0 is always the file name, 1 is the number of processor
-	String _inputFileName, _outputFileName; 
+	String _inputFileName, _outputFileName;
 	int _noOfProcessors;
 	int _noOfCores = -1; // by default number of cores is not set = it is sequential.
-	boolean _visualisation = false; // by default visualisation is off 
+	boolean _visualisation = false; // by default visualisation is off
 
 	/**
-	 * Main program. It takes arguments from the user input and pass them to other methods to process. 
+	 * Main program. It takes arguments from the user input and pass them to other methods to process.
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 		new TaskScheduler(args);
 	}
-	
+
 	public TaskScheduler(String[] args) throws Exception{
 		parseArguments(args);
 		//startExecution();
 		confirmOptionsAndExecute();
-		
+
 	}
 	/**
-	 * Runs scheduler once user confirms the options selected. 
+	 * Runs scheduler once user confirms the options selected.
 	 * Firstly program reads the input file using input reader and creates the graph.
 	 * Using the graph, the program creates a correct schedule with the shortest time.
-	 * And it generates an output file for the created schedule. 
-	 * @throws Exception 
+	 * And it generates an output file for the created schedule.
+	 * @throws Exception
 	 */
 	private void startExecution() throws Exception {
 		//InputReader ir = new InputReader(DIRECTORY + _inputFileName);
 		InputReader ir = new InputReader(_inputFileName); //input file must be in same directory as jar file
 		Graph graph = ir.readFile();
 		graph.setUpForMakingSchedules();
-		Scheduler scheduler = new Scheduler(_noOfProcessors);	
+		Scheduler scheduler = new Scheduler(_noOfProcessors);
 		Schedule s =  scheduler.getOptimalSchedule(_visualisation);
 		OutputWriter ow = new OutputWriter(_outputFileName, graph, s);
 		ow.writeToFile();
-		
+
 		//Temporary check for the output
 		String output = "Last Vertex = " + s.getLastUsedVertex().getName() +"\t|Time Taken = "+s.getTimeOfSchedule() + "\t|Note = - means empty\t|Format= Vertex:time"
 							+"\n"+ s.toString();
 		System.out.println(output);
 	}
-	
+
 	/**
-	 * Parses given arguments and stores them as variables. Invalid argument 
+	 * Parses given arguments and stores them as variables. Invalid argument
 	 * throws an appropriate exceptions. When an exception gets thrown,
 	 * an appropriate error message is provided and the program is halted.
 	 * @param args
@@ -84,7 +84,7 @@ public class TaskScheduler {
 		} catch (ArrayIndexOutOfBoundsException e){ // when the argument is not given
 			System.out.println(FILENAME_NOT_GIVEN);
 			System.exit(0);
-		} catch (FileNotFoundException e) { // when the file cannot be located 
+		} catch (FileNotFoundException e) { // when the file cannot be located
 			System.out.println(INVALID_FILENAME);
 			System.exit(0);
 		}
@@ -101,13 +101,13 @@ public class TaskScheduler {
 			checkAdditionalOptions(args);
 		}
 	}
-	
+
 	/**
-	 * Checks the additional option arguments. 
-	 * Depending on the option type, user may or may not need to pass an additional 
-	 * argument to specify option value. Therefore this method selectively checks for 
-	 * each option type and the required arguments.If any part of the arguments is 
-	 * incorrect, an appropriate error message is provided and the program is halted. 
+	 * Checks the additional option arguments.
+	 * Depending on the option type, user may or may not need to pass an additional
+	 * argument to specify option value. Therefore this method selectively checks for
+	 * each option type and the required arguments.If any part of the arguments is
+	 * incorrect, an appropriate error message is provided and the program is halted.
 	 * @param args
 	 */
 	private void checkAdditionalOptions(String[] args) {
@@ -129,14 +129,14 @@ public class TaskScheduler {
 			} else { // the argument was not an option selector therefore invalid argument
 				System.out.println(INVALID_OPTION);
 				System.exit(0);
-			}				
+			}
 		}
 	}
-	
+
 	/**
 	 * Checks if the correct value was given for the option chosen by user.
 	 * -p should be followed by an integer value.
-	 * -o should be followed by a filename. 
+	 * -o should be followed by a filename.
 	 * if values are not given correctly, an appropriate exception gets thrown.
 	 * @param option
 	 * @param value
@@ -151,14 +151,14 @@ public class TaskScheduler {
 			_outputFileName = value + ".dot";
 		}
 	}
-	
+
 	/**
 	 * Prints off the selected options before the scheduler starts executing.
 	 * User confirms by pressing y which then allows the scheduler to start execution.
 	 * If user does not press y, the program is halt and has to be re-executed.
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	private void confirmOptionsAndExecute() throws Exception { 
+	private void confirmOptionsAndExecute() throws Exception {
 		// let user to review what options have been chosen and confirm
 		System.out.println("*********************************************************************");
 		System.out.println("The Input Graph to be scheduled is: " + _inputFileName);
@@ -175,9 +175,9 @@ public class TaskScheduler {
 		}
 		System.out.println("The Name of Output File is to be: " + _outputFileName);
 		System.out.println("*********************************************************************");
-		System.out.println(CONFIRMATION_MESSAGE);		
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // get user confirmation 
+		System.out.println(CONFIRMATION_MESSAGE);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // get user confirmation
 		String line;
 		try {
 			if ((line = br.readLine()).equals("y")) { // user confirms the options by pressing "y"
@@ -187,6 +187,6 @@ public class TaskScheduler {
 				System.exit(0);
 			}
 		} catch (IOException e) {
-		}		
+		}
 	}
 }
--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,15 +1,20 @@
 package scheduler;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.PriorityBlockingQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingWorker;
 
 import graph.Graph;
 import graph.Vertex;
-import visualisation.Visualiser;
 import components.ScheduleComparator;
 /**
  * This Class uses the Schedule Class and Processor Class
@@ -23,16 +28,41 @@ import components.ScheduleComparator;
  * @author Alex Yoo
  *
  */
-public class Scheduler {
+public class Scheduler extends JFrame {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 	private int _numberOfProcessors;
 	private PriorityBlockingQueue<Schedule> _openSchedules;
 	private List<Schedule> _closedSchedules;
+	private Schedule _optimalSchedule;
+
+	private JLabel countLabel1 = new JLabel("0");
 
 	public Scheduler(int numberOfProcessors) {
+		super("Optimal Task Schedule Generator");
+		setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+
+		  gc.fill = GridBagConstraints.NONE;
+
+		  gc.gridx = 0;
+		  gc.gridy = 0;
+		  gc.weightx = 1;
+		  gc.weighty = 1;
+		  add(countLabel1, gc);
+
+		  setSize(200, 400);
+		  setDefaultCloseOperation(EXIT_ON_CLOSE);
+		  setVisible(true);
+
 		_openSchedules = new PriorityBlockingQueue<Schedule>(Graph.getInstance().getVertices().size(), new ScheduleComparator());
 		_closedSchedules = new ArrayList<Schedule>();
 		_numberOfProcessors = numberOfProcessors;
 	}
+
+
 
 	/**
 	 * This method returns the optimal schedule
@@ -41,112 +71,150 @@ public class Scheduler {
 	 */
 	public Schedule getOptimalSchedule(boolean visualisation) throws Exception {
 		if (visualisation) {
-			Visualiser.launch(Visualiser.class, new String[0]); //This should only be called once throughout the duration of the application
+
 		}
 		this.addRootVerticesSchedulesToOpenSchedule();
 
-		Schedule optimalSchedule = this.makeSchedulesUsingAlgorithm();
-
-		return optimalSchedule;
-	}
-	/**
-	 * This method uses the A* algorithm to create schedules
-	 * It only returns back once it finds an optimal schedule
-	 * It throws an exception if the openschedule queue is empty because that is not suppose to happen
-	 *
-	 * @return optimal schedule
-	 * @throws Exception
-	 */
-	private Schedule makeSchedulesUsingAlgorithm() throws Exception {
-		while(!_openSchedules.isEmpty()) {
-			Schedule currentSchedule = _openSchedules.peek();
-
-			//System.out.println("Vertex = " + currentSchedule.getLastUsedVertex().getName() +"\t|Time Taken = "+currentSchedule.getTimeOfSchedule()
-			//		+"\n"+ currentSchedule.toString());
-
-			_openSchedules.remove(currentSchedule);
-			_closedSchedules.add(currentSchedule);
-			//System.out.println("Size: "+_openSchedules.size());
-
-			if(this.hasScheduleUsedAllPossibleVertices(currentSchedule)) {
-				return currentSchedule;
-			}
-
-			this.addCurrentSchedulePossibleSuccessorsToOpenSchedule(currentSchedule);
-
+		AlgorithmWorker worker = new AlgorithmWorker();
+		worker.execute();
+		while (worker.getState() != SwingWorker.StateValue.DONE) {
+			//Wait for worker to finish
 		}
-		throw new Exception("Tried to access empty openschedules :(");
+		return _optimalSchedule;
 	}
 
-	/**
-	 * For the current schedule we are processing,
-	 * it tries to find successor schedules that are available
-	 * Those successors schedules are then added to the open schedule if it
-	 * passes the conditions required in checkScheduleOnOpenSchedule method
-	 *
-	 * @param currentSchedule
-	 */
-	private void addCurrentSchedulePossibleSuccessorsToOpenSchedule(Schedule currentSchedule) {
-		List<Vertex> currentVertexSuccessors = currentSchedule.getChildVertices();
-		for(Vertex childVertex : currentVertexSuccessors) {
-			// Preparation to make schedules for child vertex
-			Schedule currentScheduleCopy = new Schedule(currentSchedule);
-			Schedule[] currentChildVertexSchedules = new Schedule[_numberOfProcessors];
-			// all possible schedules of child vertex on the current schedule
-			currentChildVertexSchedules = currentScheduleCopy.generateAllPossibleScheduleForSpecifiedVertex(childVertex);
+	public class AlgorithmWorker extends SwingWorker<Schedule, Schedule> {
 
-			for(int i = 0; i < _numberOfProcessors; i++ ) {
-				if(this.checkScheduleOnOpenSchedule(currentChildVertexSchedules[i])) {
-					_openSchedules.add(currentChildVertexSchedules[i]);
+		@Override
+		protected Schedule doInBackground() throws Exception {
+			while(!_openSchedules.isEmpty()) {
+				Schedule currentSchedule = _openSchedules.peek();
+				publish(currentSchedule);
+				//System.out.println("Vertex = " + currentSchedule.getLastUsedVertex().getName() +"\t|Time Taken = "+currentSchedule.getTimeOfSchedule()
+				//		+"\n"+ currentSchedule.toString());
+
+				_openSchedules.remove(currentSchedule);
+				_closedSchedules.add(currentSchedule);
+				//System.out.println("Size: "+_openSchedules.size());
+
+				if(this.hasScheduleUsedAllPossibleVertices(currentSchedule)) {
+					return currentSchedule;
+				}
+
+				this.addCurrentSchedulePossibleSuccessorsToOpenSchedule(currentSchedule);
+
+			}
+			throw new Exception("Tried to access empty openschedules :(");
+		}
+
+		protected void process(List<Schedule> chunks) {
+			Schedule currentSchedule = chunks.get(chunks.size()-1);
+			countLabel1.setText("Vertex = " + currentSchedule.getLastUsedVertex().getName() +"\t|Time Taken = "+currentSchedule.getTimeOfSchedule()+"\n"+ currentSchedule.toString());
+		}
+
+		protected void done() {
+			try {
+				Schedule optimalSchedule = get();
+				_optimalSchedule = optimalSchedule;
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		/**
+		 * For the current schedule we are processing,
+		 * it tries to find successor schedules that are available
+		 * Those successors schedules are then added to the open schedule if it
+		 * passes the conditions required in checkScheduleOnOpenSchedule method
+		 *
+		 * @param currentSchedule
+		 */
+		private void addCurrentSchedulePossibleSuccessorsToOpenSchedule(Schedule currentSchedule) {
+			List<Vertex> currentVertexSuccessors = currentSchedule.getChildVertices();
+			for(Vertex childVertex : currentVertexSuccessors) {
+				// Preparation to make schedules for child vertex
+				Schedule currentScheduleCopy = new Schedule(currentSchedule);
+				Schedule[] currentChildVertexSchedules = new Schedule[_numberOfProcessors];
+				// all possible schedules of child vertex on the current schedule
+				currentChildVertexSchedules = currentScheduleCopy.generateAllPossibleScheduleForSpecifiedVertex(childVertex);
+
+				for(int i = 0; i < _numberOfProcessors; i++ ) {
+					if(this.checkScheduleOnOpenSchedule(currentChildVertexSchedules[i])) {
+						_openSchedules.add(currentChildVertexSchedules[i]);
+					}
 				}
 			}
 		}
-	}
-	/**
-	 * This method checks if the successor schedules have the conditions required to
-	 * get added into the openschedule.
-	 * It also checks inside the openschedule if there are any schedules that can be taken out
-	 * that was made redundant by successor schedule
-	 *
-	 * returns true if it passes
-	 * otherwise returns false
-	 *
-	 * @param childSchedule
-	 * @return
-	 */
-	private boolean checkScheduleOnOpenSchedule(Schedule childSchedule) {
-		boolean passesCondition = true;
-		for(Schedule schedule : _closedSchedules) {
-			if(childSchedule.getTimeOfSchedule() == schedule.getTimeOfSchedule()) {
-				if(this.isList1EqualToList2InNoOrder(childSchedule.getAllUsedVertices(), schedule.getAllUsedVertices())) {
-					passesCondition = false;
-					break;
-				}
-			}
-		}
-		for(Schedule schedule : _openSchedules) {
-			if(childSchedule.getTimeOfSchedule() == schedule.getTimeOfSchedule()) {
-				if(this.isList1EqualToList2InNoOrder(childSchedule.getAllUsedVertices(), schedule.getAllUsedVertices())) {
-					passesCondition = false;
-					break;
-				}
-			}
-		}
-		return passesCondition;
-	}
-	/**
-	 * Checks if the list is the same, does not have to be in order
-	 *
-	 * @param l1
-	 * @param l2
-	 * @return
-	 */
-	public static <T> boolean isList1EqualToList2InNoOrder(List<T> l1, List<T> l2) {
-	    final Set<T> s1 = new HashSet<>(l1);
-	    final Set<T> s2 = new HashSet<>(l2);
 
-	    return s1.equals(s2);
+		/**
+		 * This method checks if the successor schedules have the conditions required to
+		 * get added into the openschedule.
+		 * It also checks inside the openschedule if there are any schedules that can be taken out
+		 * that was made redundant by successor schedule
+		 *
+		 * returns true if it passes
+		 * otherwise returns false
+		 *
+		 * @param childSchedule
+		 * @return
+		 */
+		private boolean checkScheduleOnOpenSchedule(Schedule childSchedule) {
+			boolean passesCondition = true;
+			for(Schedule schedule : _closedSchedules) {
+				if(childSchedule.getTimeOfSchedule() == schedule.getTimeOfSchedule()) {
+					if(this.isList1EqualToList2InNoOrder(childSchedule.getAllUsedVertices(), schedule.getAllUsedVertices())) {
+						passesCondition = false;
+						break;
+					}
+				}
+			}
+			for(Schedule schedule : _openSchedules) {
+				if(childSchedule.getTimeOfSchedule() == schedule.getTimeOfSchedule()) {
+					if(this.isList1EqualToList2InNoOrder(childSchedule.getAllUsedVertices(), schedule.getAllUsedVertices())) {
+						passesCondition = false;
+						break;
+					}
+				}
+			}
+			return passesCondition;
+		}
+
+		/**
+		 * Checks if the list is the same, does not have to be in order
+		 *
+		 * @param l1
+		 * @param l2
+		 * @return
+		 */
+		public <T> boolean isList1EqualToList2InNoOrder(List<T> l1, List<T> l2) {
+		    final Set<T> s1 = new HashSet<>(l1);
+		    final Set<T> s2 = new HashSet<>(l2);
+
+		    return s1.equals(s2);
+		}
+
+		/**
+		 * This method checks if the current schedule is a finished schedule
+		 *
+		 * returns true if it is
+		 * otherwise returns false
+		 *
+		 * @param currentSchedule
+		 * @return
+		 */
+		private boolean hasScheduleUsedAllPossibleVertices(Schedule currentSchedule) {
+			List<Vertex> currentScheduleUsedVertices = currentSchedule.getAllUsedVertices();
+			for(Vertex vertex : Graph.getInstance().getVertices()) {
+				if(!currentScheduleUsedVertices.contains(vertex)) {
+					return false;
+				}
+			}
+			return true;
+		}
 	}
+
+
 	/**
 	 * This method adds root schedules to openschedules
 	 * since at the start of schedule the first task is the same no matter which processor
@@ -162,23 +230,5 @@ public class Scheduler {
 
 			_openSchedules.add(rootSchedules[0]);
 		}
-	}
-	/**
-	 * This method checks if the current schedule is a finished schedule
-	 *
-	 * returns true if it is
-	 * otherwise returns false
-	 *
-	 * @param currentSchedule
-	 * @return
-	 */
-	private boolean hasScheduleUsedAllPossibleVertices(Schedule currentSchedule) {
-		List<Vertex> currentScheduleUsedVertices = currentSchedule.getAllUsedVertices();
-		for(Vertex vertex : Graph.getInstance().getVertices()) {
-			if(!currentScheduleUsedVertices.contains(vertex)) {
-				return false;
-			}
-		}
-		return true;
 	}
 }
